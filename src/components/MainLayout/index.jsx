e9bdb547f3e3ac52d6dba03b4981bba5f67d040c
Danswar <daniel.padrino@daniel.com>
2020-09-15 23:46:34 -0300

    basic styled of layout

--- src/components/MainLayout/index.jsx
+++ src/components/MainLayout/index.jsx
@@ -8,14 +8,15 @@ import { oneOfType, arrayOf, node } from "prop-types";
 // Internal dependencies
 import Header from "components/Header";
 import Footer from "components/Footer";
+import styled from "./styles";
 
 const MainLayout = ({ children }) => {
   return (
-    <>
+    <styled.LayoutContainer>
       <Header data-test="header" />
-      {children}
+      <styled.ContentContainer>{children}</styled.ContentContainer>
       <Footer data-test="footer" />
-    </>
+    </styled.LayoutContainer>
   );
 };
 
---
+++ src/components/MainLayout/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export default {
+  LayoutContainer: styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+  `,
+  ContentContainer: styled.div`
+    flex: 1;
+  `,
+};
